6b8f2c52b13c60b79580c5c4e692d4240f8ead33
Anne-SophieBaudru <146732290+Anne-SophieBaudru@users.noreply.github.com>
2023-10-21 15:21:20 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import pandas as pd 
+import matplotlib.pyplot as plt
+import numpy as np 
+
+
+table= pd.read_csv('life_expectancy.csv',index_col=0)
+
+bel1980=table.loc['Belgium','1980']
+
+table.plot.scatter('1960','2014')
+
+#years becomes the index 
+table2=table.transpose()
+
+be=table
+
+plt.xlabel("Date")
+plt.ylabel("Values")
+plt.title("Sample Time Series Plot")
+pandas.plot()
+.to excel()
+
